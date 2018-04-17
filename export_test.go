fe8da97b0c7865c45329d789fc37d98266d1a4b7
yangwenmai <yangwen.yw@gmail.com>
2018-04-17 16:04:15 +0800

    gofmt

--- export_test.go
+++ export_test.go
@@ -3,6 +3,7 @@ package main
 import "testing"
 import . "github.com/smartystreets/goconvey/convey"
 
+// TestAdd test Add func
 func TestAdd(t *testing.T) {
 	Convey("test add", t, func() {
 		a := 3
